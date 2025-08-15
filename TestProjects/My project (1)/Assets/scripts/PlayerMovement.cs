73a1932c7c52d8edca421b03d04ec43c27db4259
Jessie-uq <jingying.gu@student.uq.edu.au>
2025-08-15 13:39:40 +1000

    20250815
    create a repo

---
+++ TestProjects/My project (1)/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
